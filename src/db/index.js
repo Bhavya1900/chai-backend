7b3991a62c2cea1b1f9e0e8f428e0c9a60ea938c
Bhavya1900 <bhavyadhoka2006@gmail.com>
2025-10-29 21:46:04 +0530

    CONNECT TO MONGO ATLAS

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import express from  'express'
+import { DB_NAME } from '../constants.js'
+import mongoose from 'mongoose';
+
+const connectDB= async()=>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MONGODB connected!! DB HOST:${connectionInstance.connection.host}`);
+
+    } catch (error) {
+        console.log("ERROR: ",error);
+        //throw error;
+        process.exit(1)
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv')..config({path: './env})
+import mongoose from 'mongoose'
+import {DB_NAME} from './constants.js'
+import connectDB from './db/index.js'
+
+import dotenv from "dotenv"
+dotenv.config({
+    path: './env'
+})
+connectDB();
+
+
+
+
+
+
+/*import express from 'express'
+
+const app=express();
+//use of IIFE's FOR DATBASE CONNECTIONS
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error)=>{
+            console.log("ERROR: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.error("error: ",error)
+        throw error
+    }
+})()*/
