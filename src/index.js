cf7d7686b11aac84914c2aefffda43d2bc31e24c
Bhavya1900 <bhavyadhoka2006@gmail.com>
2025-12-26 17:50:34 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,53 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from 'express'
+
+const app=express()
+/*function connectDB(){}
+
+connectDB();
+
+//method which uses iife
+
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error",(err)=>{
+            console.log("Error!!! ",err);
+            throw err
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("Error: ", error)
+        throw error
+    }
+})()*/
